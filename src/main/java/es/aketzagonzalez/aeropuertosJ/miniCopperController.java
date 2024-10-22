70f499b0447aa8ea6818b1ad0ce8076a3907007b
FormedNumber95 <152279572+FormedNumber95@users.noreply.github.com>
2024-10-22 12:19:27 +0200

    Initial commit

---
+++ src/main/java/es/aketzagonzalez/aeropuertosJ/miniCopperController.java
@@ -0,0 +1,70 @@
+package es.aketzagonzalez.aeropuertosJ;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class miniCopperController {
+
+    @FXML
+    private ToggleButton btnBlazingRed;
+
+    @FXML
+    private ToggleButton btnElectricBlue;
+
+    @FXML
+    private ToggleButton btnLapisluxuryBlue;
+
+    @FXML
+    private ToggleButton btnMidnightBlack;
+
+    @FXML
+    private ToggleButton btnMoonwalkGrey;
+
+    @FXML
+    private ToggleButton btnPepperWhite;
+
+    @FXML
+    private ToggleButton btnThunderGray;
+
+    @FXML
+    private ToggleButton btnVolcaninOrange;
+
+    @FXML
+    private ToggleGroup grupoColores;
+
+    @FXML
+    private ToggleGroup grupoColores1;
+
+    @FXML
+    private ToggleGroup grupoColores2;
+
+    @FXML
+    private ToggleGroup grupoColores3;
+
+    @FXML
+    private ToggleGroup grupoColores4;
+
+    @FXML
+    private ToggleGroup grupoColores5;
+
+    @FXML
+    private ToggleGroup grupoColores6;
+
+    @FXML
+    private ToggleGroup grupoColores7;
+
+    @FXML
+    private ImageView imgActivarLuces;
+
+    @FXML
+    private ImageView imgLuces;
+
+    @FXML
+    void toggleLuces(MouseEvent event) {
+
+    }
+
+}
