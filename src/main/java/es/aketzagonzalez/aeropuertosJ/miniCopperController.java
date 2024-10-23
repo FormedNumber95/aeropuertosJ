a0358dbc80846a3c764dce045632a1d7c6d4108e
FormedNumber95 <152279572+FormedNumber95@users.noreply.github.com>
2024-10-23 12:31:40 +0200

    casi acabado el ejercicio
    faltan los comentarios y si se quiere comprobar el backgroundcolor para que se vea cual esta elegido
    23/10/2024

--- src/main/java/es/aketzagonzalez/aeropuertosJ/miniCopperController.java
+++ src/main/java/es/aketzagonzalez/aeropuertosJ/miniCopperController.java
@@ -3,6 +3,7 @@ package es.aketzagonzalez.aeropuertosJ;
 import javafx.fxml.FXML;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
@@ -40,15 +41,54 @@ public class miniCopperController {
 
     @FXML
     private ImageView imgLuces;
+    
+    @FXML
+    private ImageView imgCoche;
 
     @FXML
     void cambiarColor(MouseEvent event) {
-
+    	if(btnBlazingRed.isSelected()) {
+    		System.out.println("AQUI");
+    		imgCoche.setImage(new Image(getClass().getResource("/imagenes/coches/miniBlazingRed.png").toString()));
+    	}else {
+    		if(btnElectricBlue.isSelected()) {
+    			imgCoche.setImage(new Image(getClass().getResource("/imagenes/coches/miniElectricBlue.png").toString()));
+    		}else {
+    			if(btnLapisluxuryBlue.isSelected()) {
+    				imgCoche.setImage(new Image(getClass().getResource("/imagenes/coches/miniLapisluxuryBlue.png").toString()));
+    			}else {
+    				if(btnMidnightBlack.isSelected()) {
+    					imgCoche.setImage(new Image(getClass().getResource("/imagenes/coches/miniMidnightBlack.png").toString()));
+    				}else {
+    					if(btnMoonwalkGrey.isSelected()) {
+    						imgCoche.setImage(new Image(getClass().getResource("/imagenes/coches/miniMoonwalkGrey.png").toString()));
+    					}else {
+    						if(btnPepperWhite.isSelected()) {
+    							imgCoche.setImage(new Image(getClass().getResource("/imagenes/coches/miniPepperWhite.png").toString()));
+    						}else {
+    							if(btnThunderGray.isSelected()) {
+    								imgCoche.setImage(new Image(getClass().getResource("/imagenes/coches/miniThunderGray.png").toString()));
+    							}else {
+    								if(btnVolcaninOrange.isSelected()) {
+    									imgCoche.setImage(new Image(getClass().getResource("/imagenes/coches/miniVolcaninOrange.png").toString()));
+    								}
+    							}
+    						}
+    					}
+    				}
+    			}
+    		}
+    	}
     }
 
     @FXML
     void toggleLuces(MouseEvent event) {
+    	if(imgLuces.isVisible()) {
+    		imgActivarLuces.setImage(new Image(getClass().getResourceAsStream("/imagenes/lucesOff.png")));
+    	}else {
+    		imgActivarLuces.setImage(new Image(getClass().getResourceAsStream("/imagenes/lucesOn.png")));
+    	}
     	imgLuces.setVisible(!imgLuces.isVisible());
     }
-
+    
 }
